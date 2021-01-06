df0a9e4d8362f24c6e6f3449deb66c3a16426a88
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-07 00:40:20 +0100

    :chart_with_upwards_trend: Add react-ga

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,23 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ReactGA from 'react-ga'
 import CourseRegister from './pages/EventRegister'
 import Dashboard from 'pages/Dashboard'
 import Player from 'pages/Player'
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/event/register" component={CourseRegister} />
+const Router = () => {
+  ReactGA.initialize('UA-186693543-1')
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/player" component={Player} />
-    </Switch>
-  </BrowserRouter>
-)
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/event/register" component={CourseRegister} />
+
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/player" component={Player} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 export default Router
