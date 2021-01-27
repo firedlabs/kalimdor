6ea59d7dc162782d833cb6d4e85ef17968fe38e3
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-27 21:23:33 +0100

    :zap: Add the first classes videos

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -13,94 +13,55 @@ const dataFollow = {
     'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
   lessons: [
     {
-      title: 'Carregando...',
-      videos: [
-        {
-          src:
-            'https://player.vimeo.com/external/502166017.hd.mp4?s=b6b52061d00a692e9c8ed41778759a4b7b0a4669&profile_id=169&v=1',
-          title: 'Carregando 1...',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
-          type: 'video/mp4'
-        },
-        {
-          src:
-            'https://player.vimeo.com/external/502166017.hd.mp4?s=b6b52061d00a692e9c8ed41778759a4b7b0a4669&profile_id=169&v=2',
-          title: 'Carregando 2...',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
-          type: 'video/mp4'
-        },
-        {
-          src:
-            'https://player.vimeo.com/external/502166017.hd.mp4?s=b6b52061d00a692e9c8ed41778759a4b7b0a4669&profile_id=169&v=3',
-          title: 'Carregando 3...',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
-          type: 'video/mp4'
-        }
-      ]
-    }
-  ]
-}
-
-const dataSub = {
-  sources: {
-    src:
-      'https://player.vimeo.com/external/490348141.hd.mp4?s=e473c83ba63929f71334bd4167fcf8be3fd59930&profile_id=175',
-
-    type: 'video/mp4'
-  },
-  poster:
-    'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
-  lessons: [
-    {
-      title: 'Módulo 0',
+      title: 'Módulo 00',
       videos: [
         {
           src:
             'https://player.vimeo.com/external/490348141.hd.mp4?s=e473c83ba63929f71334bd4167fcf8be3fd59930&profile_id=175',
           title: 'Projeto do curso',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1027992089.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490348213.hd.mp4?s=65af3df8e8f3339abf933b0cb1aea8e9a67c587e&profile_id=175',
           title: 'Pré-requisitos do curso',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028703945.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490348393.hd.mp4?s=8734fcb0db1e45cde958fe2e34daea395c18e9b1&profile_id=175',
           title: 'Onde tirar suas dúvidas',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028704335.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490348510.hd.mp4?s=b3c3e588fc32b8189be1d866ae27e43ef311f457&profile_id=175',
           title: 'Como funciona o VSCode',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028704690.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490349544.hd.mp4?s=c25d851552a45d6799188b6983df9b8bcd2ec549&profile_id=175',
           title: 'Figma para Devs',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028705279.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490349755.hd.mp4?s=daca5c077323b3bc97caa615b5c2dcbcef9a8080&profile_id=175',
           title: 'Revisão',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028705790.jpg',
           type: 'video/mp4'
         },
         {
           src:
             'https://player.vimeo.com/external/490349849.hd.mp4?s=5a01556b0267414d24988bcff741c7da270c7df4&profile_id=175',
           title: 'Desafios top top top',
-          poster: 'https://i.vimeocdn.com/video/1038997437.jpg',
+          poster: 'https://i.vimeocdn.com/video/1028706127.jpg',
           type: 'video/mp4'
         }
       ]
@@ -108,9 +69,11 @@ const dataSub = {
   ]
 }
 
+const dataSub = dataFollow
+
 function Player() {
   const [data, setData] = useState(dataFollow)
-  const [lessonActive, setLessonActive] = useState('Carregando...')
+  const [lessonActive, setLessonActive] = useState('Módulo 00')
 
   useEffect(
     () =>
@@ -120,10 +83,10 @@ function Player() {
 
           setData(dataSub)
 
-          setLessonActive('Módulo 0')
+          setLessonActive('Módulo 00')
         } catch (err) {
           setData(dataFollow)
-          setLessonActive('Carregando...')
+          setLessonActive('Módulo 00')
         }
       })(),
     []
