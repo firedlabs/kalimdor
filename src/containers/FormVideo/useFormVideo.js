572c09b40969aa60a295262862e1b531c4cb26cf
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-31 17:00:12 +0100

    :construction: Send video to api

--- src/containers/FormVideo/useFormVideo.js
+++ src/containers/FormVideo/useFormVideo.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
 import VideoService from 'services/VideoService'
 
 function useFormVideo() {
+  const history = useHistory()
   const { register, handleSubmit, watch } = useForm()
   const [activeLoading, setActiveLoading] = useState(false)
   const [activeModalError, setActiveModalError] = useState(false)
@@ -16,6 +18,7 @@ function useFormVideo() {
       setActiveLoading(true)
       await VideoService.save(video)
       setActiveLoading(false)
+      history.push('/')
     } catch (err) {
       setActiveLoading(false)
       setActiveModalError(true)
