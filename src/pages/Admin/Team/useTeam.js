2d8aea7f9c4169d68be661c863b04dfea0e253d4
Marco Bruno <marco.bruno.br@gmail.com>
2021-03-11 11:09:08 +0100

    :sparkles: Create the team list

--- src/pages/Admin/Team/useTeam.js
+++ src/pages/Admin/Team/useTeam.js
@@ -1,8 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import TeamService from 'services/TeamService'
 
 function useTeam() {
   const columns = ['Nome', 'Editar']
-  const [data] = useState([['Carregando...', 'Carregando...']])
+  const [data, setData] = useState([['Carregando...', 'Carregando...']])
+
+  useEffect(
+    () =>
+      (async () => {
+        try {
+          const { data } = await TeamService.getAll()
+          setData(() =>
+            data.map(({ id, name }) => [
+              name,
+              { as: Link, to: `/admin/team/${id}`, content: 'Editar' }
+            ])
+          )
+        } catch (err) {
+          console.log('err', err)
+        }
+      })(),
+    []
+  )
 
   return { data, columns }
 }
--- src/services/TeamService.js
+++ src/services/TeamService.js
@@ -1,6 +1,7 @@
 import api from './api'
 const TeamService = {}
 
+TeamService.getAll = async () => await api.get('/team')
 TeamService.save = async (team) => await api.post('/team', JSON.stringify(team))
 
 export default TeamService
